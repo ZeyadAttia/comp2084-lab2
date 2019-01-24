f84c2df268743461c4065a6a736323f04c7840a5
ZeyadAttia <38919070+ZeyadAttia@users.noreply.github.com>
2019-01-23 23:09:18 -0500

    Making a list of categories, that on click will show which category is clicked.

---
+++ Lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(string categoryName) {
+            // it will be null only if the user entered this link (GET: Categories/Details) directly in the url and without the required parameters as a query string, not using the provided links.
+            if (categoryName != null) {
+                ViewBag.CategoryNameMessage = "You selected category: " + categoryName;
+            }
+            return View();
+        }
+    }
+}
---
+++ Lab2/README.md
@@ -0,0 +1,7 @@
+﻿<h1>COMP2084 - Lab 2</h1>
+<p>
+    I added some stuff to handle the situation if the user puts the url that goes directly to the 
+    Categories/Details view without putting any query string params, instead of using the links i made
+    to reach this view file.
+    <br><br><small>Made by Zeyad Attia</small>
+</p>
